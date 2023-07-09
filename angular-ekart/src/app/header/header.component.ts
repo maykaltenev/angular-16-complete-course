0a8c1fd181a6c889e15f1b2a5f211516b04247fa
Maykal Tenev <michaeltenev@gmail.com>
2023-07-09 14:59:38 +0200

    inner components

--- angular-ekart/src/app/header/header.component.ts
+++ angular-ekart/src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: '<h3>eKart</h3>',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {}
